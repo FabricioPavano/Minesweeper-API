d579211084d67fa62a000bc52c87c2907ae2a60e
Fabricio Pavano <faripavano@gmail.com>
2020-11-07 13:33:04 -0300

    added game scaffold, added rack-cors

---
+++ db/migrate/20201107162441_create_games.rb
@@ -0,0 +1,11 @@
+class CreateGames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :games do |t|
+      t.integer :rows
+      t.integer :cols
+      t.integer :mines
+
+      t.timestamps
+    end
+  end
+end
